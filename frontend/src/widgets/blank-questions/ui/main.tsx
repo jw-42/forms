a4f3b61c9afd6e50b420e88f31ffa943835a3e1e
jw-42 <laukman_13@mail.ru>
2025-07-01 12:09:23 +0300

    [feat]: add form properties and integrate edit functionality

--- frontend/src/widgets/blank-questions/ui/main.tsx
+++ frontend/src/widgets/blank-questions/ui/main.tsx
@@ -6,11 +6,13 @@ import React, { useState } from "react"
 import { QuestionBuilder } from "./builder"
 import { QuestionFooter } from "./footer"
 import { List } from "@shared/ui"
+import { useForm } from "@entities/form/hooks"
 
 export const BlankQuestions = () => {
 
   const params = useParams<'id'>()
 
+  const { data: form } = useForm(params?.id)
   const { data: questions } = useQuestions(params?.id)
 
   const [answers, setAnswers] = useState<Record<string, string>>({})
@@ -25,12 +27,13 @@ export const BlankQuestions = () => {
   return (
     <React.Fragment>
       <List
-        after={<QuestionBuilder/>}
+        after={form?.can_edit && <QuestionBuilder/>}
         afterCondition={questions && questions.length < 5}
       >
         {questions?.map((question) => (
           <QuestionItem
             {...question}
+            readOnly={form?.is_answered}
             value={answers[question.id] || ''}
             onChange={(value) => handleAnswerChange(question.id, value)}
           />
