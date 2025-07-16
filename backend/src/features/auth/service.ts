fd48d6e53a794fa891c3449d60e8f690f0e92291
jw-42 <laukman_13@mail.ru>
2025-07-16 03:45:22 +0300

    [refactor]: remove vk-io integration for user notifications in AuthService

--- backend/src/features/auth/service.ts
+++ backend/src/features/auth/service.ts
@@ -3,7 +3,6 @@ import type { LaunchParams } from './types'
 import { ApiError } from '@shared/utils'
 import { sign } from 'hono/jwt'
 import { AuthRepository } from './repository'
-import { API } from 'vk-io'
 
 class AuthService {
   private readonly clientSecret: string;
@@ -56,19 +55,6 @@ class AuthService {
 
     if (!user) {
       user = await this.authRepository.createUser(vk_user_id)
-
-      const api = new API({
-        token: `${Bun.env.VK_GROUP_TOKEN}`
-      })
-
-      const userInfo = await api.users.get({
-        user_ids: [vk_user_id]
-      })
-
-      await api.messages.send({
-        peer_id: 374811416,
-        message: `Новый пользователь: ${userInfo[0]?.first_name} ${userInfo[0]?.last_name}`
-      })
     }
 
     if (user.is_banned) {
